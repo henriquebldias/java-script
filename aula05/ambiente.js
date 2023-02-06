a2db85e3c3e922f8836c58d4266d8718378ee2be
Henrique <henriquebldias@gmail.com>
2023-02-06 16:47:11 -0300

    variáveis compostas

---
+++ aula05/ambiente.js
@@ -0,0 +1,11 @@
+let num = [4, 6, 12, 1, 5, 9]
+console.sort()
+
+//num.lenght - tamanho
+//num.sort() - ordena
+//num.push() - insere
+//num.indexOf() - posição  
+
+for(let pos in num) {
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+}

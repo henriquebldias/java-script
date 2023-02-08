c9b768e7aac903a200dd8958c37def401b779a9a
Henrique <henriquebldias@gmail.com>
2023-02-08 13:09:51 -0300

    fotos

--- projeto_horadodia/script.js
+++ projeto_horadodia/script.js
@@ -1,19 +1,19 @@
 function carregar() {
     var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('imagem')
+    var foto = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.toLocaleTimeString()
     setInterval(carregar, 1000)
     msg.innerHTML = `Agora são ${hora}.`
     if (hora >= 0 && hora < 12) {
         //BOM DIA!
-        img.src = ''
+        foto.src = 'manha.jpg'
     } else if(hora >= 12 && hora < 18){
         //BOA TARDE!
-        img.src = 'tarde.jpg'
+        foto.src = 'tarde.jpg'
     } else {
         //BOA NOITE!
-        img.src = 'noite.jpg'
+        foto.src = 'noite.jpg'
     }
 }
 
